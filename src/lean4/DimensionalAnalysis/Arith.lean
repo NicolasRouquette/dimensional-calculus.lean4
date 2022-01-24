bd513e5e1b0d4e684d3dc9dcbc6798a42454d67a
Nicolas Rouquette <Nicolas.F.Rouquette@jpl.nasa.gov>
2022-01-23 17:27:49 -0800

    WIP

--- src/lean4/DimensionalAnalysis/Arith.lean
+++ src/lean4/DimensionalAnalysis/Arith.lean
@@ -1,21 +1,47 @@
 -- Arithmetic for dimensional calculus
 namespace DimensionalAnalysis
 
+structure Rational where
+  (isPositive: Bool)
+  (num: Nat)
+  (den: Nat)
+  deriving Repr
+
+namespace Rational
+
+def simplify (r: Rational) : Rational :=
+  let c := Nat.gcd r.num r.den
+  Rational.mk r.isPositive (r.num / c) (r.den / c)
+
+def multiply (r1 r2: Rational) : Rational :=
+  let n12 := Nat.mul r1.num r2.num
+  let d12 := Nat.mul r1.den r2.den
+  let c := Nat.gcd n12 d12
+  let f := r1.isPositive && r2.isPositive
+  Rational.mk f (n12/c) (d12/c)
+
+def divide (r1 r2: Rational) : Rational :=
+  multiply r1 (Rational.mk r2.isPositive r2.den r2.num)
+
+#eval multiply (Rational.mk true 4 7) (Rational.mk false 21 3) 
+
+end Rational
+
 inductive DCalc : Type
   | symbol : String -> DCalc
   | mul : DCalc -> DCalc -> DCalc
   | div : DCalc -> DCalc -> DCalc
-  | power : DCalc -> Bool -> Nat -> Nat -> DCalc
+  | power : DCalc -> Rational -> DCalc
   deriving Repr
 
 declare_syntax_cat dcalc
 
-syntax ident : dcalc -- DCalc.symbol
-syntax:70 dcalc:70 "*" dcalc:71 : dcalc -- DCalc.mul
-syntax:80 dcalc:80 "/" dcalc:81 : dcalc -- DCalc.div
+syntax ident : dcalc
+syntax dcalc "*" dcalc : dcalc
+syntax dcalc "/" dcalc : dcalc
 syntax "(" dcalc ")" : dcalc
-syntax:90 dcalc:90 "^" num "/" num : dcalc -- DCalc.power
-syntax:90 dcalc:90 "^" "-" num "/" num : dcalc -- DCalc.power
+syntax dcalc "^" num "/" num : dcalc
+syntax dcalc "^" "-" num "/" num : dcalc
 
 -- auxiliary notation for translating `dcalc` into `term`
 syntax "`[DCalc| " dcalc "]" : term
@@ -25,47 +51,45 @@ macro_rules
   | `(`[DCalc| $x:dcalc * $y:dcalc ]) => `(DCalc.mul `[DCalc| $x] `[DCalc| $y])
   | `(`[DCalc| $x:dcalc / $y:dcalc ]) => `(DCalc.div `[DCalc| $x] `[DCalc| $y])
   | `(`[DCalc| ($x:dcalc) ]) => `(`[DCalc| $x ])
-  | `(`[DCalc| $x:dcalc ^ $n:numLit / $d:numLit ]) => `(DCalc.power `[DCalc| $x] true $n $d)
-  | `(`[DCalc| $x:dcalc ^ - $n:numLit / $d:numLit ]) => `(DCalc.power `[DCalc| $x] false $n $d)
+  | `(`[DCalc| $x:dcalc ^ $n:numLit / $d:numLit ]) => `(DCalc.power `[DCalc| $x] (Rational.mk true $n $d))
+  | `(`[DCalc| $x:dcalc ^ - $n:numLit / $d:numLit ]) => `(DCalc.power `[DCalc| $x] (Rational.mk false $n $d))
 
-#check `[DCalc| x ]           -- DCalc.symbol "x" : DCalc
-#check `[DCalc| (x) ]         -- DCalc.symbol "x" : DCalc
-#check `[DCalc| x * y ]       -- DCalc.mul (DCalc.symbol "x") (DCalc.symbol "y") : DCalc
-#check `[DCalc| x / y ]       -- DCalc.div (DCalc.symbol "x") (DCalc.symbol "y") : DCalc
-#check `[DCalc| x ^ 1/1 ]     -- DCalc.power (DCalc.symbol "x") true 1 1 : DCalc
-#check `[DCalc| x^-1/1 ]      -- DCalc.power (DCalc.symbol "x") false 1 1 : DCalc
-#check `[DCalc| x*y^2/1]      -- DCalc.mul (DCalc.symbol "x") (DCalc.power (DCalc.symbol "y") true 2 1) : DCalc
-#check `[DCalc| x^3/1*y^2/1]  -- DCalc.mul (DCalc.power (DCalc.symbol "x") true 3 1) (DCalc.power (DCalc.symbol "y") true 2 1) : DCalc
+#check `[DCalc| x ] 
+#check `[DCalc| (x) ] 
+#check `[DCalc| x * y ] 
+#check `[DCalc| x / y ]
+#check `[DCalc| x ^ 1/1 ]
+#check `[DCalc| x^-1/1 ]
+#check `[DCalc| x*y^2/1]
+#check `[DCalc| x^3/1*y^2/1]
 
 structure DCalcFactor where
   (symbol: String)
-  (isPositive: Bool)
-  (num: Nat)
-  (den: Nat)
+  (exp: Rational)
   deriving Repr
 
 inductive DCalcExpr : Type
   | factors: Array DCalcFactor -> DCalcExpr
   | mul : DCalcExpr -> DCalcExpr -> DCalcExpr
   | div : DCalcExpr -> DCalcExpr -> DCalcExpr
-  | power : DCalcExpr -> Bool -> Nat -> Nat -> DCalcExpr
+  | power : DCalcExpr -> Rational -> DCalcExpr
   deriving Repr
 
 def convert : DCalc -> DCalcExpr
-  | DCalc.symbol x => DCalcExpr.factors #[ (DCalcFactor.mk x true 1 1) ]
+  | DCalc.symbol x => DCalcExpr.factors #[ (DCalcFactor.mk x (Rational.mk true 1 1)) ]
   | DCalc.mul x y => DCalcExpr.mul (convert x) (convert y)
   | DCalc.div x y => DCalcExpr.div (convert x) (convert y)
-  | DCalc.power x f n d => DCalcExpr.power (convert x) f n d
+  | DCalc.power x exp=> DCalcExpr.power (convert x) exp
 
 def simplify : DCalcExpr -> DCalcExpr
   | DCalcExpr.factors fs => 
     DCalcExpr.factors fs
-  | DCalcExpr.power x f d n => 
+  | DCalcExpr.power x e => 
     match x with
     | DCalcExpr.factors fs => 
-      DCalcExpr.power (DCalcExpr.factors fs) f d n
+      DCalcExpr.power (DCalcExpr.factors fs) e
     | x' => 
-      DCalcExpr.power x' f d n
+      DCalcExpr.power x' e
   | d => d
 
 #eval simplify (convert `[DCalc| x ^ 7/2 ] )
