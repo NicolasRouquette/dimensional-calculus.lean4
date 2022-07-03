70d6b80d7d6aa7a9749d66fb053f77f56f0dc0ef
Nicolas Rouquette <Nicolas.F.Rouquette@jpl.nasa.gov>
2022-07-02 19:25:36 -0700

    lake update

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,14 @@
 import Lake
-open System Lake DSL
+open Lake DSL
 
-package DA where
-  packagesDir := FilePath.mk "build.lean4_packages"
-  srcDir := FilePath.mk "src" / "lean4"
-  buildDir := FilePath.mk "build.lean4"
-  dependencies := #[{
-    name := `mathlib
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "master"
-  }]
+package da where
+  packagesDir := "build.lean4_packages"
+  srcDir := "src" / "lean4"
+  buildDir := "build.lean4"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[defaultTarget]
+lean_exe da where
+  root := `DA.Main
